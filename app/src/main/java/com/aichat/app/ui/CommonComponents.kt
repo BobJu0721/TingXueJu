ea3c71eede5febfcc4558f6d5c0905e347331dcb
BobJu0721 <ju100091bobju@gmail.com>
2026-08-26 18:11:37 +0800

    補 TextAlign import

--- app/src/main/java/com/aichat/app/ui/CommonComponents.kt
+++ app/src/main/java/com/aichat/app/ui/CommonComponents.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
